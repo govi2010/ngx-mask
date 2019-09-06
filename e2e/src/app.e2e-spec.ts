23a5c5f2bc1064d1de9a580f9ca3b92e5d35b967
“isysenko” <sip31296@gmail.com>
2019-09-06 17:59:04 +0300

    add e2e tests

---
+++ e2e/src/app.e2e-spec.ts
@@ -0,0 +1,28 @@
+import { AppPage } from './app.po';
+import { browser, logging, element, by } from 'protractor';
+
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getTitleText()).toEqual('Angular\nMask Plugin');
+  });
+
+  it('input text', () => {
+    let elem = element(by.css('input'));
+    elem.sendKeys('12345678');
+    expect(elem.getAttribute('value')).toEqual('12/34/5678');
+  });
+
+  afterEach(async () => {
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
